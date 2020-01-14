89b00704bff391b0597bcc1543c108cc3f1ca013
marinagotovkina <marina@uppercase.no>
2020-01-14 14:03:22 +0100

    - add task_3 Navigation

---
+++ task_3/app.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+const express = require('express');
+
+const app = express();
+
+const mainRoutes = require('./routes/main');
+const usersRoutes = require('./routes/users');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(mainRoutes);
+app.use(usersRoutes);
+
+app.listen(3000);
+
+
+
+
